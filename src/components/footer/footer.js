9d9951fac49c5ef0995c25a46cd3ea68443db84a
Mehul Thakkar <mehulthakkar02@gmail.com>
2019-08-11 15:45:57 +0530

    Added s3 deployment script

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -20,8 +20,7 @@ export default class Footer extends Component {
             }
           </ul>
           <ul className="copyright">
-            <li>© Copyright 2014 CeeVee</li>
-            <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+            <li>© Copyright 2019 Mehul Thakkar</li>
           </ul>
         </div>
         <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open" /></a></div>
