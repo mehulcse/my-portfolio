5246ab033eb4e46c3c73544f271b67a7cd149674
Mehul Thakkar <mehulthakkar02@gmail.com>
2019-08-11 18:06:53 +0530

    Updated header style

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -11,7 +11,7 @@ export default class Header extends Component {
                         <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
                         <a className="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
                         <ul id="nav" className="nav">
-                            <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
+                            <li><a className="smoothscroll" href="#home">Home</a></li>
                             <li><a className="smoothscroll" href="#about">About</a></li>
                             <li><a className="smoothscroll" href="#resume">Resume</a></li>
                         </ul>
