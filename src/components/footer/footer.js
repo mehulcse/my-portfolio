03bc30c2e96d945c201a3aae859060216f73617e
Mehul Thakkar <mehulthakkar02@gmail.com>
2020-02-24 08:41:19 +0530

    Updated footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -20,7 +20,7 @@ export default class Footer extends Component {
             }
           </ul>
           <ul className="copyright">
-            <li>© Copyright 2019 Mehul Thakkar</li>
+            <li>© Copyright {new Date().getFullYear()} Mehul Thakkar</li>
           </ul>
         </div>
         <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open" /></a></div>
